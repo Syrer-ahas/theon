dd4f04f8a63fd7c083598696d58ebeeb24963a4c
Syrer-ahas <alkhidirea@gmail.com>
2026-09-18 12:39:47 +0000

    Sep 18, 2026, 2:39 PM

--- auth-config.example.js
+++ auth-config.example.js
@@ -3,6 +3,5 @@
  * A Google client ID is public. Never put a Google client secret in a website.
  */
 window.TACTICAL_AUTH_CONFIG = {
-  googleClientId: 'YOUR_GOOGLE_OAUTH_WEB_CLIENT_ID.apps.googleusercontent.com',
-  localOrigin: 'http://localhost:3000'
+  googleClientId: 'YOUR_GOOGLE_OAUTH_WEB_CLIENT_ID.apps.googleusercontent.com'
 };
--- auth-config.js
+++ auth-config.js
@@ -1,5 +1,4 @@
 /* Google OAuth browser configuration. Replace the value with your Web client ID. */
 window.TACTICAL_AUTH_CONFIG = {
-  googleClientId: '794145126379-162ulmelngbk9jq6jhke54q4smp9u3tl.apps.googleusercontent.com',
-  localOrigin: 'http://localhost:3000'
+  googleClientId: '794145126379-162ulmelngbk9jq6jhke54q4smp9u3tl.apps.googleusercontent.com'
 };
